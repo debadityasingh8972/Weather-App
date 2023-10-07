f31336c0355e9fd662a5ff3573eae88bf21ffad1
Debaditya <debadityasingh9@gmail.com>
2023-10-07 18:28:03 +0530

    added countries list drop down from country-state-city package

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -1,8 +1,26 @@
+"use client";
+
 import React from 'react'
+
+import Select from 'react-select';
+import { Country, City } from 'country-state-city';
+
+const options = Country.getAllCountries().map(country => ({
+    value: {
+        latitude: country.latitude,
+        longitude: country.longitude,
+        isoCode: country.isoCode,
+    },
+    label: country.name,
+}))
 
 const CityPicker = () => {
     return (
-        <div>CityPicker</div>
+        <div>
+            <Select
+                options={options}
+            />
+        </div>
     )
 }
 
